1b4425dbdda1fd9c78f6e32747d9807275f27649
Tapan Prakash <tapanprakasht@gmail.com>
2018-04-26 11:10:57 +0530

    Refactoring- Part 3

--- GildedRose/GildedRose.swift
+++ GildedRose/GildedRose.swift
@@ -12,21 +12,79 @@ enum ITEM:String {
   case CONJURED  = "Conjured Mana Cake"
 }
 
+public protocol ItemProtocol {
+  func updateItem(item: Item) -> Item
+}
+
+public class AgedBrie: ItemProtocol {
+  public func updateItem(item: Item) -> Item {
+    var newItem: Item = item
+    if newItem.quality < 50 {
+      newItem.quality = newItem.quality + 1
+    }
+    newItem.sellIn =  newItem.sellIn - 1
+    if newItem.sellIn < 0 {
+      if newItem.quality < 50 {
+        newItem.quality = newItem.quality + 1
+      }
+    }
+    return newItem
+  }
+}
+
+public class BackStage: ItemProtocol {
+  public func updateItem(item: Item) -> Item {
+    var newItem: Item = item
+    if newItem.quality < 50 {
+      newItem.quality = newItem.quality + 1
+      if newItem.sellIn < 11 {
+        newItem.quality = newItem.quality + 1
+      }
+      if newItem.sellIn < 6 {
+        newItem.quality = newItem.quality + 1
+      }
+    }
+    newItem.sellIn =  newItem.sellIn - 1
+    if newItem.sellIn < 0 {
+      newItem.quality = newItem.quality - newItem.quality
+    }
+    return newItem
+  }
+}
+
+public class Conjured: ItemProtocol {
+  public func updateItem(item: Item) -> Item {
+    var newItem: Item = item
+    if newItem.sellIn > 0 {
+      newItem.quality = newItem.quality - 2
+    } else {
+      newItem.quality = newItem.quality - 4
+    }
+    if newItem.quality < 0 {
+      newItem.quality = 0
+    }
+    newItem.sellIn =  newItem.sellIn - 1
+    
+    return newItem
+  }
+}
+
 public class GildedRose {
 
+   public static let itemMap = ["Aged Brie":AgedBrie(),"Backstage passes to a TAFKAL80ETC concert": BackStage(),"Conjured Mana Cake": Conjured()] as [String: ItemProtocol]
+  
     public static func updateQuality(items: Array<Item>) -> [Item] {
         var items = items
         for i in 0 ..< items.count {
-          switch items[i].name {
-              case ITEM.AGED_BRIE.rawValue:  items[i] = updateAgedBrie(item: items[i])
-              case ITEM.BACKSTAGE.rawValue:  items[i] = updateBackstage(item: items[i])
-              case ITEM.CONJURED.rawValue:   items[i] = updateConjured(item: items[i])
-              default:                      items[i] = updateNormalItems(item: items[i])
+          let itemDelegate = itemMap[items[i].name]
+          if itemDelegate != nil {
+            items[i] = (itemDelegate?.updateItem(item: items[i]))!
+          } else {
+            items[i] = updateNormalItems(item: items[i])
           }
         }
         return items
     }
-  
     public static func updateNormalItems(item: Item) -> Item {
       var newItem: Item = item
       if newItem.quality > 0 && newItem.name != ITEM.SULFURAS.rawValue{
@@ -40,52 +98,4 @@ public class GildedRose {
       }
       return newItem
     }
-  
-    public static func updateAgedBrie(item: Item) -> Item {
-      var newItem: Item = item
-      if newItem.quality < 50 {
-        newItem.quality = newItem.quality + 1
-      }
-      newItem.sellIn =  newItem.sellIn - 1
-      if newItem.sellIn < 0 {
-        if newItem.quality < 50 {
-          newItem.quality = newItem.quality + 1
-        }
-      }
-      return newItem
-    }
-  
-    public static func updateBackstage(item: Item) -> Item {
-      var newItem: Item = item
-      if newItem.quality < 50 {
-        newItem.quality = newItem.quality + 1
-        if newItem.sellIn < 11 {
-          newItem.quality = newItem.quality + 1
-        }
-        if newItem.sellIn < 6 {
-          newItem.quality = newItem.quality + 1
-        }
-      }
-      newItem.sellIn =  newItem.sellIn - 1
-      if newItem.sellIn < 0 {
-        newItem.quality = newItem.quality - newItem.quality
-      }
-      return newItem
-    }
-  
-    public static func updateConjured(item: Item) -> Item {
-      var newItem: Item = item
-      if newItem.sellIn > 0 {
-        newItem.quality = newItem.quality - 2
-      } else {
-        newItem.quality = newItem.quality - 4
-      }
-      if newItem.quality < 0 {
-        newItem.quality = 0
-      }
-      newItem.sellIn =  newItem.sellIn - 1
-      
-      return newItem
-    }
-  
 }
